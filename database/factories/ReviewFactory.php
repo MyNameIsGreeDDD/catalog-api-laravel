e849201c376cb9f2a2a3be543a8d0748324271a2
greeddd <MyName1sGreeDDD@gmail.com>
2023-02-18 21:22:49 +0300

    db seeds

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => fake()->name(),
+            'text' => fake()->text(100),
+            'product_id' => Product::factory()
+                ->has(User::factory()->count(mt_rand(1, 3)),'users')
+                ->create()->id
+        ];
+    }
+}
