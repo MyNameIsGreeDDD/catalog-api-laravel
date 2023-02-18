8dc7426b01991dfdea9cf5b4c2381af5237165d0
greeddd <MyName1sGreeDDD@gmail.com>
2023-02-18 21:22:28 +0300

    database

---
+++ database/migrations/2023_02_18_180128_create_favorites_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('product_id')
+                ->on('products')
+                ->references('id')
+                ->cascadeOnDelete();
+
+            $table->foreign('user_id')
+                ->on('users')
+                ->references('id')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
