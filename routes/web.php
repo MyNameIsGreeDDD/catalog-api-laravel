0cf202afe03d95ae212d11305bc3937ab3e60b70
greeddd <MyName1sGreeDDD@gmail.com>
2023-02-20 00:58:38 +0300

    favorite api
    delete pld route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('favorite/{product}/{user}', [ProductController::class, 'addFavorite'])
-    ->name('admin.favorite.store');
