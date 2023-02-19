e0f69d08516f2b097213eecd872408bdfbd6a13b
greeddd <MyName1sGreeDDD@gmail.com>
2023-02-19 07:20:09 +0300

    product view update, add show route for products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,4 +15,10 @@ class ProductController extends Controller
 
         return view('admin.products.index', compact('products'));
     }
+
+    public function show(Product $product): Factory|View|Application
+    {
+        return view('admin.products.show', compact('product'));
+    }
+
 }
