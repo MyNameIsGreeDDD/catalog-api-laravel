f6d772f940a5dddd02635822ed4d9b1799c11bdd
greeddd <MyName1sGreeDDD@gmail.com>
2023-02-20 14:17:49 +0300

    Revert "review index"
    This reverts commit 756b9c0e1f461cfc7d566a5d6c96b5aa85672cbb.

--- database/migrations/2023_02_18_180105_create_reviews_table.php
+++ database/migrations/2023_02_18_180105_create_reviews_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -17,8 +18,6 @@ return new class extends Migration {
             $table->string('first_name');
             $table->mediumText('text');
             $table->unsignedBigInteger('product_id');
-
-            $table->index(['first_name', 'text', 'product_id'], 'review_index');
 
             $table->foreign('product_id')
                 ->on('products')
